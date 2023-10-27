3fb442c92d49d138b67dc937b9bae33e576c928b
sweco-SE1A2O <johan.froberg@sweco.se>
2023-10-27 09:27:46 +0200

    Automation and added more images

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const panorama = new PANOLENS.ImagePanorama( 'images/Holmsnäset_Pan_134.png' );
-let imageContainer = document.querySelector('.image-container');
+const images = ['images/Holmsnäset_Pan_134.png', 'images/Storsands_camping_Pan_77.png', 'images/Storflon_Pan_162.png', 'images/Hammaren_Pan_128.png', 'images/Storsands_camping (1).jpg'];
 
-const viewer = new PANOLENS.Viewer({
-    container: imageContainer,
-    controlBar: true,
-});
-
-viewer.add( panorama);
-viewer.OrbitControls.noZoom = true;
+for(let i = 0; i < images.length; i++){
+    eval("panorama" + i + "= new PANOLENS.ImagePanorama( images[i] );")
+    let imageContainer = document.querySelector('.image-container' + i);
+    window['viewer' + i] = new PANOLENS.Viewer({
+        container: imageContainer,
+        controlBar: true,
+    });
+    window['viewer' + i].add( window['panorama' + i]);
+    window['viewer' + i].OrbitControls.noZoom = true;
+    }
 
